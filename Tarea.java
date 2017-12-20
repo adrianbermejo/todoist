b9d591176999fd64a55978d2b069b6f08e652825
adrian bermejo <adrianbermejogom@gmail.com>
2017-12-20 13:36:01 +0100

    Refactorización Todoist

---
+++ Tarea.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Tarea
+{
+   // Almacena nuestras tareas
+    private ArrayList<String> tareas; 
+
+    public Tarea()
+    {
+        tareas = new ArrayList<String>();
+    }
+    
+}
